7243b2f90eab56bd397a0492e5fd45b73ab3971d
anatoliy <anatolijdegtyar@yandex.ru>
2021-07-27 18:23:13 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-$('.floors').hover (
+/*$('.floors').hover (
   function() {
     $('.apartment__popup').fadeIn();
   },
   function() {
     $('.apartment__popup').fadeOut(50);
   }
-);
+);*/
+
+  $('.floors').each(function() {
+  let ths = $(this);
+  ths.hover(
+    function() {
+      $('.apartment__popup').fadeIn();
+    },
+    function() {
+      $('.apartment__popup').fadeOut(50);
+    })
+});
