45d363a3e33a3468f12ff680002c46deb031d68f
anatoliy <anatolijdegtyar@yandex.ru>
2021-07-28 08:51:37 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-/*$('.floors').hover (
+$('.floors').hover (
   function() {
     $('.apartment__popup').fadeIn();
   },
   function() {
     $('.apartment__popup').fadeOut(50);
   }
-);*/
+);
 
-  $('.floors').each(function() {
-  let ths = $(this);
-  ths.hover(
-    function() {
-      $('.apartment__popup').fadeIn();
-    },
-    function() {
-      $('.apartment__popup').fadeOut(50);
-    })
+$(document).ready(function(){
+  $('.img_wr').mousemove(function(e){
+    // положение элемента
+    let pos = $(this).offset();
+    let elem_left = pos.left;
+    let elem_top = pos.top;
+    // положение курсора внутри элемента
+    let Xinner = e.pageX - elem_left;
+    let Yinner = e.pageY - elem_top;
+    if (Xinner < 466) {
+      $('.apartment__popup').css('right','180px');
+    } else {
+      $('.apartment__popup').css('right','760px');
+    }
+  });
 });
